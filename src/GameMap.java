8542a313b2570c0c9080e0bf8d146bfbc4ac6cf8
Mathew <m.matejsvarc@gmail.com>
2025-02-20 11:10:45 +0100

    Map works now

--- src/GameMap.java
+++ src/GameMap.java
@@ -4,15 +4,34 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class GameMap{
+    private String lokace = "Fight02";
   public boolean loadMap(){
       try (BufferedReader br = new BufferedReader(new FileReader("Map.txt"))){
           String line;
           while((line = br.readLine())!=null){
               String[] lines = line.split(" ");
-              for (int i = 0; i < 6; i++) {
-                  if(!Objects.equals(lines[i], "null")){
-                      if(!Objects.equals(lines[i],"-")) {
-                          System.out.println(lines[i]);
+              if(Objects.equals(lokace, lines[0])) {
+                  for (int i = 0; i < 6; i++) {
+                      if (!Objects.equals(lines[i], "null")) {
+                          if (!Objects.equals(lines[i], "-")) {
+                              switch(i){
+                                  case 0:
+                                      System.out.println("Currenty in room: "+lines[0]);
+                                      break;
+                                  case 2:
+                                      System.out.println("To the left is: "+lines[2]);
+                                      break;
+                                  case 3:
+                                      System.out.println("Above is: "+lines[3]);
+                                      break;
+                                  case 4:
+                                      System.out.println("To the right is: "+lines[4]);
+                                      break;
+                                  case 5:
+                                      System.out.println("On the bottom is: "+lines[5]);
+                                      break;
+                              }
+                          }
                       }
                   }
               }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
   GameMap gameMap = new GameMap();
-        System.out.println(gameMap.loadMap());
+        System.out.println("///////\n"+gameMap.loadMap());
     }
 }
